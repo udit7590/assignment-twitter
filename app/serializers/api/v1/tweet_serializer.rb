d0eff42269e2cd755a1e610236e22e52a1e9426a
Udit Mittal <uditmittal@Udits-MacBook-Pro.local>
2020-03-08 16:58:46 +0530

    Adds API to fetch follows and followers for a user

--- app/serializers/api/v1/tweet_serializer.rb
+++ app/serializers/api/v1/tweet_serializer.rb
@@ -1,4 +1,8 @@
 class Api::V1::TweetSerializer < ActiveModel::Serializer
+  class UserSerializer < ActiveModel::Serializer
+    attributes :id
+  end
+
   attributes :id, :content
   belongs_to :user
 end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,19 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  class FollowSerializer < ActiveModel::Serializer
+    attributes :email, :user_id
+
+    def email
+      object.followable.try(:email)
+    end
+
+    def user_id
+      object.followable.try(:id)
+    end
+  end
+
+  attributes :id, :email, :follows, :followers
+
+  has_many :follows, serializer: Api::V1::UserSerializer::FollowSerializer
+  has_many :followers, serializer: Api::V1::UserSerializer::FollowSerializer
+  
 end
