116f5ba0ad934523b065a32e8501acbac6e5ab99
Udit Mittal <uditmittal@Udits-MacBook-Pro.local>
2020-03-08 16:41:52 +0530

    Allows tweets to be sorted by create date in api

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -3,6 +3,7 @@ module Api
     class TweetsController < Api::ApiController
       def me
         @tweets = current_user.tweets_from_follows
+        @tweets = @tweets.order(created_at: ((params[:sort] == 'asc') ? :asc : :desc))
         render json: @tweets
       end
     end
